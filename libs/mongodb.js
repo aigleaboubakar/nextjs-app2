aa93680cc6c0ae20d5923e1653d3a845ee8835c6
aigle aboubakar <mahamataboubakar08@gmail.com>
2025-04-03 14:57:56 +0100

    desploy project

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -9,7 +9,7 @@ const connectMongoDB = async () => {
 
   try {
     const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
-    const connectionString = `mongodb+srv://integrationninjas:${password}@test.vjuinp4.mongodb.net/?retryWrites=true&w=majority`; // clustore url
+    const connectionString = `mongodb+srv://mahamataboubakar08:${password}@aiglecluster.vzd60tg.mongodb.net/?retryWrites=true&w=majority&appName=AigleCluster`; // clustore url
     await mongoose.connect(connectionString, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
